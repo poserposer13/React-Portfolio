5a1ee59e355c3cd52721c74ad42a34874614f489
Sean Kempf <poserposer@me.com>
2021-01-14 18:25:06 -0700

    Created Basic Contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,31 @@
+import ContactCard from "../components/ContactCard"
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
+function Contact() {
+    return (
+        <>
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <ContactCard />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ContactCard />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ContactCard />
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
+
+
+export default Contact
